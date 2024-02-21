a5ccc039accdf60f367826b96735f648c7325cce
PatriciaPena <patri.datw@gmail.com>
2024-02-21 09:52:05 +0100

    DS

--- view/app/partials/header.php
+++ view/app/partials/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Noticias de Grifendor</title>
+    <title>Noticias de GrifendorRRR</title>
 </head>
 
 <body>
